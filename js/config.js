25b22a9a8cbb3819ebc9605c646c38f2c052326a
Toms Buls <spectrofly.tbu@gmail.com>
2026-02-25 17:15:08 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,13 @@
+window.SORA_CONFIG = {
+  portalUrl: "https://spectrofly.maps.arcgis.com",
+
+  // We will fill this in after we register the app in ArcGIS Online
+  oauthAppId: "PASTE_CLIENT_ID_HERE",
+
+  // Your existing web map
+  webmapId: "545efda8fc7e44aaae8d5a7ea967e3b3",
+
+  // We will fill these in after you create the new hosted layers
+  soraBuffersLayerUrl: "PASTE_BUFFERS_LAYER_URL_HERE",
+  remotePilotsLayerUrl: "PASTE_REMOTE_PILOTS_LAYER_URL_HERE"
+};
