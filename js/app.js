846b286cfc24d6a69d5a194bec4dff6fd44c29fe
Toms Buls <spectrofly.tbu@gmail.com>
2026-02-26 14:07:32 +0100

    Update app.js
    adding widgets

--- js/app.js
+++ js/app.js
@@ -3,9 +3,22 @@ require([
   "esri/identity/OAuthInfo",
   "esri/identity/IdentityManager",
   "esri/WebMap",
-  "esri/views/MapView"
-], function (esriConfig, OAuthInfo, esriId, WebMap, MapView) {
-
+  "esri/views/MapView",
+  "esri/widgets/LayerList",
+  "esri/widgets/BasemapGallery",
+  "esri/widgets/Bookmarks",
+  "esri/widgets/Measurement"
+], function (
+  esriConfig,
+  OAuthInfo,
+  esriId,
+  WebMap,
+  MapView,
+  LayerList,
+  BasemapGallery,
+  Bookmarks,
+  Measurement
+) {
   const cfg = window.SORA_CONFIG;
 
   // Tell ArcGIS which portal we are using
@@ -44,3 +57,34 @@ require([
 
   view.when(updateAuthStatus);
 });
+  // -------------------------------
+  // WIDGETS
+  // -------------------------------
+
+  view.when(function () {
+
+    // Layer List
+    const layerList = new LayerList({
+      view: view,
+      container: "widgetLayerList"
+    });
+
+    // Basemap Gallery
+    const basemapGallery = new BasemapGallery({
+      view: view,
+      container: "widgetBasemap"
+    });
+
+    // Bookmarks
+    const bookmarks = new Bookmarks({
+      view: view,
+      container: "widgetBookmarks"
+    });
+
+    // Measurement
+    const measurement = new Measurement({
+      view: view,
+      container: "widgetMeasure"
+    });
+
+  });
