a456f816ab8c56d6efdd231c1e1bf1a92d575725
Toms Buls <spectrofly.tbu@gmail.com>
2026-02-26 11:56:25 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.SORA_CONFIG = {
   portalUrl: "https://spectrofly.maps.arcgis.com",
 
   // We will fill this in after we register the app in ArcGIS Online
-  oauthAppId: "PASTE_CLIENT_ID_HERE",
+  oauthAppId: "I2ObuC4B37VkJB6Y",
 
   // Your existing web map
   webmapId: "545efda8fc7e44aaae8d5a7ea967e3b3",
